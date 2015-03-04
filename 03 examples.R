f15d29da6f1144d364fbec2a7adcd503f6552c49
CodyStumpo <codybass@gmail.com>
2015-03-03 17:06:47 -0800

    finished examples

--- 03 examples.R
+++ 03 examples.R
@@ -1,11 +1,14 @@
 # make some example meals and analyze.
+save(nutrition, file="out/nutrition.rda")
+save(nutrition2, file="out/nutrition2.rda")
+
 
 mySteakBowl = c("Steak", "White Rice", "Pinto Beans", "Lettuce", "Guacamole", "Red Tomatillo Salsa")
 mySteakBurrito = c("Steak", "White Rice", "Pinto Beans", "Lettuce", "Guacamole", "Red Tomatillo Salsa", "Soft Flour Tortilla")
 myChickenBowl = c("Chicken", "White Rice", "Pinto Beans", "Lettuce", "Sour Cream", "Tomato Salsa", "Tomato Salsa")
 mySofritasBowl = c("Sofritas", "Brown Rice", "Pinto Beans", "Sour Cream", "Corn Salsa", "Green Tomatillo Salsa")
-myCarnitasBurrito = c("Carnitas", "Pinto Beans", "Guacamole", "Green Tomatillo Salsa","Soft Flour Tortilla")
-mySteakTacos = c("Steak", "Lettuce", "Guacamole", "Red Tomatillo Salsa", "Soft Corn Tortillas")
+myCarnitasBurrito = c("Carnitas", "Pinto Beans", "White Rice", "Guacamole", "Green Tomatillo Salsa","Soft Flour Tortilla")
+mySteakTacos = c("Steak", "Lettuce", "Guacamole", "Red Tomatillo Salsa", "Tomato Salsa", "Soft Corn Tortillas")
 myChickenSalad = c("Chicken", "Lettuce", "Vinaigrette", "Black Beans", "Corn Salsa", "Cheese")
 
 
@@ -28,12 +31,24 @@ mySplits = function(meal){
   fatPercent = fatCal / calcCal
   carbPercent = carbCal / calcCal
   proteinPercent = proteinCal / calcCal
-  x=data.frame(fatPercent, carbPercent, proteinPercent)
+  x=data.frame(Calories=raw[1], fatPercent, carbPercent, proteinPercent)
   rownames(x) <- deparse(substitute(meal))
   x
 }
 
+myMeals = data.frame(Calories = double(), fatPercent = double(), carbPercent=double(), proteinPercent=double())
+myMeals=rbind(myMeals, mySplits(mySteakBowl))
+myMeals=rbind(myMeals, mySplits(mySteakBurrito))
+myMeals=rbind(myMeals, mySplits(myChickenBowl))
+myMeals=rbind(myMeals, mySplits(mySofritasBowl))
+myMeals=rbind(myMeals,  mySplits(myCarnitasBurrito))
+myMeals=rbind(myMeals,  mySplits(mySteakTacos))
+myMeals=rbind(myMeals,  mySplits(myChickenSalad))
+
+for(i in 1:7) barplot(t(myMeals[i,2:4]), beside=TRUE, names.arg=c("Fat%", "Carb%", "Prot%"), main=row.names(myMeals[i,]))
+
 x = myTots2(mySofritasBowl)
+
 #myMealTots2 = rbind(myMealTots2, myTots2(mySteakBowl))
 #row.names(myMealTots2)<-c("mySofritasBowl")
-radarchart (data.frame(rbind(rep(4,12), rep(0,12), (x[2:13]/x[1]))), seg=4)
+radarchart (data.frame(rbind(rep(4,12), rep(0,12), x[2:13]/x[1])), seg=4)
